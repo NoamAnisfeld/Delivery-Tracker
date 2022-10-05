2a74318e1240a7e3808b6f0782fc060288a45e58
NoamAnisfeld <NoamAnisfeld@gmail.com>
2022-10-06 01:10:34 +0300

    adding global state properties

--- src/GlobalState/GlobalState.tsx
+++ src/GlobalState/GlobalState.tsx
@@ -1,10 +1,18 @@
 import { createContext, useContext, useReducer } from "react";
+import type { Product } from '../API/products-list';
+
 
 interface GlobalStateInterface {
-    [key: string]: any,
+    selectedProduct: Product | null,
+    awaitedProducts: Product[],
+    archivedProducts: Product[],
 }
 
-const initialGlobalState: GlobalStateInterface = {},
+const initialGlobalState: GlobalStateInterface = {
+        selectedProduct: null,
+        awaitedProducts: [],
+        archivedProducts: [],
+    },
     GlobalStateContext = createContext({});
 
 export const useGlobalStateContext = () => useContext(GlobalStateContext);
