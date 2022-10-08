522220b50cd6f3979a741bbb668e260e90f9c7b3
NoamAnisfeld <NoamAnisfeld@gmail.com>
2022-10-08 23:09:37 +0300

    simplifying the GlobalState logic (yeah I made it far too complicated before)

--- src/GlobalState/GlobalState.tsx
+++ src/GlobalState/GlobalState.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useState } from "react";
 import type { Product } from '../interfaces/interfaces';
 
 interface GlobalStateInterface {
@@ -15,104 +15,93 @@ interface GlobalStateInterface {
     deleteItemFromArchivedProducts: (itemId: number) => void,
 }
 
-const GlobalStateContext = createContext<GlobalStateInterface | null>(null);
+const placeholderGlobalState: GlobalStateInterface = {
+    selectedProduct: null,
+    setSelectedProduct: (item: Product) => {},
+
+    awaitedProducts: [],
+    addItemToAwaitedProducts: (newItem: Product) => {},
+    deleteItemFromAwaitedProducts: (itemId: number) => {},
+
+    archivedProducts: [],
+    archiveItem: (itemId: number) => {},
+    dearchiveItem: (itemId: number) => {},
+    deleteItemFromArchivedProducts: (itemId: number) => {},
+}
+
+const GlobalStateContext = createContext(placeholderGlobalState);
 
 export const useGlobalStateContext = () => useContext(GlobalStateContext);
 
 export function GlobalStateProvider({ children }: React.PropsWithChildren) {
-    const [globalState, updateGlobalState] = useReducer(
-        (
-            prevState: GlobalStateInterface,
-            updates: Partial<GlobalStateInterface> | ( (prevState: GlobalStateInterface) => GlobalStateInterface )
-        ): GlobalStateInterface => {
-            if (typeof updates === 'function') {
-                return updates(prevState);
-            }
-            return {...prevState, ...updates}
-        }, {
-            selectedProduct: null,
-            setSelectedProduct: (item: Product) => {},
+    const
+        [selectedProduct, setSelectedProduct] = useState<Product | null>(null),
+        [awaitedProducts, setAwaitedProducts] = useState<Product[]>([]),
+        [archivedProducts, setArchivedProducts] = useState<Product[]>([]);
+    
+    
+    // ToDo: Allow adding multiple instances of the same item
 
-            awaitedProducts: [],
-            addItemToAwaitedProducts: (newItem: Product) => {},
-            deleteItemFromAwaitedProducts: (itemId: number) => {},
-
-            archivedProducts: [],
-            archiveItem: (itemId: number) => {},
-            dearchiveItem: (itemId: number) => {},
-            deleteItemFromArchivedProducts: (itemId: number) => {},
-        });
-
-    updateGlobalState({
-        setSelectedProduct: (item: Product) =>
-            updateGlobalState({ selectedProduct: item }),
-
-        addItemToAwaitedProducts: (newItem: Product) =>
-            updateGlobalState(prevState =>
-                prevState.awaitedProducts.find(item => item.id === newItem.id) ?
-                prevState :
-                {
-                    ...prevState,
-                    awaitedProdcuts: [...prevState.awaitedProducts, newItem]
-                }
-            ),
-
-        deleteItemFromAwaitedProducts: (itemId: number) =>
-            updateGlobalState(prevState =>
-                ({
-                    ...prevState,
-                    awaitedProducts: [...prevState.awaitedProducts].splice(
-                        prevState.awaitedProducts.findIndex(item => item.id === itemId),
-                        1
-                    )
-                })
-            ),
-
-        archiveItem: (itemId: number) =>
-            updateGlobalState(prevState => {
-                const
-                    { awaitedProducts, archivedProducts } = prevState,
-                    index = awaitedProducts.findIndex(item => item.id === itemId);
-                if (index === -1) {
-                    return prevState;
-                } else {
-                    return {
-                        ...prevState,
-                        awaitedProducts: [...awaitedProducts].splice(index, 1),
-                        archivedProducts: [...archivedProducts, { ...awaitedProducts[index] } ]
-                    }
-                }
-            }),
-
-        dearchiveItem: (itemId: number) =>
-            updateGlobalState(prevState => {
-                const
-                    { awaitedProducts, archivedProducts } = prevState,
-                    index = archivedProducts.findIndex(item => item.id === itemId);
-                if (index === -1) {
-                    return prevState;
-                } else {
-                    return {
-                        ...prevState,
-                        awaitedProducts: [...awaitedProducts, { ...archivedProducts[index] } ],
-                        archivedProducts: [...archivedProducts].splice(index, 1)
-                    }
-                }
-            }),
+    function addItemToAwaitedProducts(newItem: Product) {
+        if (awaitedProducts.find(item => item.id === newItem.id))
+            return;
         
-        deleteItemFromArchivedProducts: (itemId: number) =>
-            updateGlobalState(prevState =>
-                ({
-                    ...prevState,
-                    archivedProducts: [...prevState.archivedProducts].splice(
-                        prevState.archivedProducts.findIndex(item => item.id === itemId),
-                        1
-                    )
-                })
-            ),        
-    });
+        setAwaitedProducts(prev => [...prev, newItem]);
+    }
 
-    return <GlobalStateContext.Provider value={globalState}>
+    function deleteItemFromAwaitedProducts(itemId: number) {
+        const index = awaitedProducts.findIndex(item => item.id === itemId);
+        if (index === -1)
+            return;
+
+        setAwaitedProducts(prev => [...prev].splice(index, 1));
+    }
+
+    function deleteItemFromArchivedProducts(itemId: number) {
+        const index = archivedProducts.findIndex(item => item.id === itemId);
+        if (index === -1)
+            return;
+
+        setArchivedProducts(prev => [...prev].splice(index, 1));
+    }
+
+    function archiveItem (itemId: number) {
+        const awaitedIndex = awaitedProducts.findIndex(item => item.id === itemId);
+        if (awaitedIndex === -1)
+            return;
+
+        const archivedIndex = archivedProducts.findIndex(item => item.id === itemId);
+        if (archivedIndex === -1)
+            setArchivedProducts(prev => [...prev, awaitedProducts[awaitedIndex]]);
+        
+        setAwaitedProducts(prev => [...prev].splice(awaitedIndex, 1));
+    }
+
+    function dearchiveItem(itemId: number) {
+        const archivedIndex = archivedProducts.findIndex(item => item.id === itemId);
+        if (archivedIndex === -1)
+            return;
+
+        const awaitedIndex = awaitedProducts.findIndex(item => item.id === itemId);
+        if (awaitedIndex === -1)
+            setAwaitedProducts(prev => [...prev, archivedProducts[archivedIndex]]);
+
+        setArchivedProducts(prev => [...prev].splice(archivedIndex, 1));
+    }
+
+    return <GlobalStateContext.Provider value={{
+        selectedProduct,
+        awaitedProducts,
+        archivedProducts,
+
+        setSelectedProduct,
+        addItemToAwaitedProducts,
+        deleteItemFromAwaitedProducts,
+        deleteItemFromArchivedProducts,
+
+        archiveItem,
+        dearchiveItem,
+    }}>
         {children}
     </GlobalStateContext.Provider>
 }
