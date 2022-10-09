7d69af359754a751cd1e83564c29af4cf502e4b4
NoamAnisfeld <NoamAnisfeld@gmail.com>
2022-10-09 14:11:56 +0300

    layout improvements

--- src/components/ItemCard/ItemCard.tsx
+++ src/components/ItemCard/ItemCard.tsx
@@ -24,67 +24,53 @@ export default function ItemCard({
     } = useGlobalStateContext();
 
     return <Card sx={{ width: 300, height: 400, bgcolor: "secondary.dark", m: 2 }}>
-        <Typography p={2} variant="h4" component="h3" align="center">
-            {item.name}
-        </Typography>
-        <Typography p={2}>
-            {item.store}
-        </Typography>
-        <Grid container justifyContent="center">
-            <Typography variant="h6">
-                Estimated delivery date
-            </Typography>
-            <Typography variant="h4" component="p">
-                {item.estimatedDeliveryDate?.toDateString?.() || "not specified"}
-            </Typography>
-        </Grid>
-        <Grid container>
-            {// @ts-ignore until I add it
-            item.description ? <Grid item xs={true}>
-                <CardContent>
-                    <CollapsibleText>
-                        <Typography variant="body2" color="text.secondary">
-                            {// @ts-ignore until I add it
-                            item.description}
-                        </Typography>
-                    </CollapsibleText>
-                </CardContent>
-            </Grid> : undefined}
-            {// @ts-ignore until I add it
-            item.image ? <Grid item sm={3} m={2}>
-                <CardMedia
-                    component="img"
-                    sx={{ width: "100%", height: "auto" }}
-                    // @ts-ignore until I add it
-                    image={item.image}
-                    alt={item.name}
-                />
-            </Grid> : undefined}
-        </Grid>
-        <Grid container sx={{ justifyContent: "space-between", alignItems: "center" }}>
-            <Grid item xs={true}>
-                <CardActions>
-                    {context === "SelectionForm" &&
-                    <Button variant="contained" color="secondary" size="small"
-                        title="Add to delivery waiting list"
-                        onClick={() => addItemToAwaitedProducts(item)}
-                    >
-                        +
-                    </Button>}
-                    {context === "AwaitingList" &&
-                    <Button variant="contained" color="secondary" size="small"
-                        title="Remove from delivery waiting list"
-                        onClick={() => deleteItemFromAwaitedProducts(item)}
-                    >
-                        -
-                    </Button>}
-                </CardActions>
-            </Grid>
-            <Grid item sm={4}>
-                <Typography variant="h5" m={2} textAlign="center">
-                    {item.price}$
+        <Grid container direction="column" height="100%" justifyContent="space-between">
+            <Grid item>
+                <Typography p={2} variant="h4" component="h3" align="center">
+                    {item.name}
+                </Typography>
+
+                <Typography p={2}>
+                    {item.store}
                 </Typography>
             </Grid>
+            <Grid item>
+                <Grid container justifyContent="end" marginTop="auto">
+                    <Typography m={2} fontSize="2rem" textAlign="center">
+                        {item.price}$
+                    </Typography>
+                </Grid>
+
+                <Grid container justifyContent="center">
+                    <Typography variant="h6">
+                        Estimated delivery date
+                    </Typography>
+                    <Typography variant="h4" component="p">
+                        {item.estimatedDeliveryDate?.toDateString?.() || "not specified"}
+                    </Typography>
+                </Grid>
+                <Grid container justifyContent="space-between" alignItems="center" >
+                    <Grid item xs={true}>
+                        <CardActions>
+                            {context === "SelectionForm" &&
+                                <Button variant="contained" color="secondary" size="small"
+                                    title="Add to delivery waiting list"
+                                    onClick={() => addItemToAwaitedProducts(item)}
+                                >
+                                    +
+                                </Button>}
+                            {context === "AwaitingList" &&
+                                <Button variant="contained" color="secondary" size="small"
+                                    title="Remove from delivery waiting list"
+                                    onClick={() => deleteItemFromAwaitedProducts(item)}
+                                >
+                                    -
+                                </Button>}
+                        </CardActions>
+                    </Grid>
+                </Grid>
+            </Grid>
         </Grid>
+
     </Card>
 }
