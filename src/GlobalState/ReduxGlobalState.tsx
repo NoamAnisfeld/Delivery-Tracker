c17299f2bd871afe4b180a61f1891e066e708681
NoamAnisfeld <NoamAnisfeld@gmail.com>
2022-10-14 14:33:53 +0300

    creating a Redux version of the global state module

---
+++ src/GlobalState/ReduxGlobalState.tsx
@@ -0,0 +1,190 @@
+import { configureStore, createReducer, createSlice, createAction, PayloadAction } from "@reduxjs/toolkit";
+import { original } from 'immer';
+
+import { fetchProductsList } from "../external data/products-list";
+import type { ExampleProduct } from '../interfaces/interfaces';
+import PurcashedProduct from "../data structures/PurcashedProduct";
+import Currency from '../data structures/Currency'
+import type { Currencies } from "../data structures/Currency";
+import { fetchSavedLists, saveLists } from "./SaveState";
+import { pollExchangeRates } from "../external data/exchange-rates";
+
+interface GlobalStateInterface {
+    selectedExampleProduct: ExampleProduct | null,
+    exampleProducts: ExampleProduct[],
+
+    awaitedProducts: PurcashedProduct[],
+    archivedProducts: PurcashedProduct[],
+
+    cardsView: boolean,
+
+    availableCurrencies: Currencies,
+    selectedCurrency: string,
+}
+
+const placeholderGlobalState: GlobalStateInterface = {
+    selectedExampleProduct: null,
+
+    exampleProducts: [],
+    awaitedProducts: [],
+
+    archivedProducts: [],
+
+    cardsView: false,
+
+    availableCurrencies: {
+        USD: new Currency({
+            name: 'US Dollar',
+            sign: '$',
+            exchangeRates: {}
+        }),
+        ILS: new Currency({
+            name: 'Israeli Shekel',
+            sign: '₪',
+            exchangeRates: {}
+        }),
+    },
+    selectedCurrency: 'USD',
+}
+
+const
+    actionSetSelectedExampleProduct =
+        createAction<ExampleProduct | null>('selectedExampleProduct.set'),
+    actionAddItemToAwaitedProducts =
+        createAction<PurcashedProduct>('awaitedProducts.add'),
+    actionDeleteItemFromAwaitedProducts =
+        createAction<PurcashedProduct>('awaitedProducts.remove'),
+    actionArchiveItem =
+        createAction<PurcashedProduct>('awaitedProducts.archive'),
+    actionDearchiveItem = 
+        createAction<PurcashedProduct>('archivedProducts.dearchive'),
+    actionDeleteItemFromArchivedProducts =
+        createAction<PurcashedProduct>('archivedProducts.remove'),
+    actionSetCardsView =
+        createAction<boolean>('cardsView.set'),
+    actionSetSelectedCurrency =
+        createAction<string>('selectedCurrency.set'),
+
+    actionSetAwaitedProducts =
+        createAction<PurcashedProduct[]>('awaitedProducts.set'),
+    actionSetArchivedProducts =
+        createAction<PurcashedProduct[]>('archivedProducts.set'),
+    actionSetExchangeRates =
+        createAction<{ [currencyCode: string]: number }>('exchangeRates.set'),
+    actionSetExampleProducts =
+        createAction<ExampleProduct[]>('exampleProducts.set');
+
+const mainSlice = createSlice({
+    name: 'main',
+    initialState: placeholderGlobalState,
+    reducers: {
+        setSelectedExampleProduct(state, { payload }: PayloadAction<ExampleProduct>) {
+            state.selectedExampleProduct = payload;
+        },
+
+        addItemToAwaitedProducts(state, { payload }: PayloadAction<PurcashedProduct>) {
+            state.awaitedProducts.push(payload)
+            saveLists(state);
+        },
+
+        deleteItemFromAwaitedProducts(state, { payload }: PayloadAction<PurcashedProduct>) {
+            state.awaitedProducts = original(state.awaitedProducts)!.filter(
+                item => item === payload);
+            saveLists(state);
+        },
+    
+        archiveItem(state, { payload }: PayloadAction<PurcashedProduct>) {
+            state.awaitedProducts = original(state.awaitedProducts)!.filter(
+                item => item === payload);
+            state.archivedProducts.push(payload);
+            
+            saveLists(state);
+        },
+        
+        dearchiveItem(state, { payload }: PayloadAction<PurcashedProduct>) {            
+            state.archivedProducts = original(state.archivedProducts)!.filter(
+                item => item === payload);
+            state.awaitedProducts.push(payload);
+                
+            saveLists(state);
+        },
+            
+        deleteItemFromArchivedProducts(state, { payload }: PayloadAction<PurcashedProduct>) {
+            state.archivedProducts = original(state.archivedProducts)!.filter(
+                item => item === payload);
+                    
+            saveLists(state);
+        },
+                
+        setCardsView(state, { payload }: PayloadAction<boolean>) {
+            state.cardsView = payload;
+        },
+                
+        setSelectedCurrency(state, { payload }: PayloadAction<string>) {
+            state.selectedCurrency = payload;
+        },
+                
+        setAwaitedProducts(state, { payload }: PayloadAction<PurcashedProduct[]>) {
+            state.awaitedProducts = payload;
+        },
+                
+        setArchivedProducts(state, { payload }: PayloadAction<PurcashedProduct[]>) {
+            state.archivedProducts = payload;
+        },
+
+        setExchangeRates(state, { payload }: PayloadAction<{
+            [currencyCode: string]: number;
+        }>) {
+            state.availableCurrencies.USD.exchangeRates = payload;
+        },
+                
+        setExampleProducts(state, { payload }: PayloadAction<ExampleProduct[]>) {
+            state.exampleProducts = payload;
+        },
+    }
+});
+        
+const store = configureStore({
+    reducer: mainSlice.reducer
+});
+
+pollExchangeRates({
+    baseCurrency: 'USD',
+    targetCurrencies: ['ILS'],
+    onRatesAvailable: newRates =>
+        store.dispatch({ payload: newRates, type: actionSetExchangeRates })
+});
+ 
+fetchProductsList().then(list =>
+    store.dispatch({ payload: list, type: actionSetExampleProducts })
+);
+
+const {
+    awaitedProducts,
+    archivedProducts,
+} = fetchSavedLists();
+store.dispatch({ payload: awaitedProducts, type: actionSetAwaitedProducts });
+store.dispatch({ payload: archivedProducts, type: actionSetArchivedProducts });
+
+if (awaitedProducts.length || archivedProducts.length)
+    saveLists({ awaitedProducts, archivedProducts });
+
+
+export default store;
+export const
+    setSelectedExampleProduct = (product: ExampleProduct) =>
+        store.dispatch(mainSlice.actions.setSelectedExampleProduct),
+    addItemToAwaitedProducts = (item: PurcashedProduct) =>
+        store.dispatch(mainSlice.actions.setSelectedExampleProduct),
+    deleteItemFromAwaitedProducts = (item: PurcashedProduct) =>
+        store.dispatch(mainSlice.actions.deleteItemFromAwaitedProducts),
+    archiveItem = (item: PurcashedProduct) =>
+        store.dispatch(mainSlice.actions.archiveItem),
+    dearchiveItem = (item: PurcashedProduct) =>
+        store.dispatch(mainSlice.actions.dearchiveItem),
+    deleteItemFromArchivedProducts = (item: PurcashedProduct) =>
+        store.dispatch(mainSlice.actions.deleteItemFromArchivedProducts),
+    setCardsView = (useCardsView: boolean) =>
+        store.dispatch(mainSlice.actions.setCardsView),
+    setSelectedCurrency = (currencyCode: string) =>
+        store.dispatch(mainSlice.actions.setSelectedCurrency);
