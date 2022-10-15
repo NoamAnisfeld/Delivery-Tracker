ec388888a1b28606886f27a3ba615e7378948257
NoamAnisfeld <NoamAnisfeld@gmail.com>
2022-10-16 00:24:51 +0300

    more fixes - now it works

--- src/GlobalState/ReduxGlobalState.tsx
+++ src/GlobalState/ReduxGlobalState.tsx
@@ -53,7 +53,7 @@ const mainSlice = createSlice({
     name: 'main',
     initialState: placeholderGlobalState,
     reducers: {
-        setSelectedExampleProduct(state, { payload }: PayloadAction<ExampleProduct>) {
+        setSelectedExampleProduct(state, { payload }: PayloadAction<ExampleProduct | null>) {
             state.selectedExampleProduct = payload;
         },
 
@@ -148,18 +148,18 @@ if (awaitedProducts.length || archivedProducts.length)
 export default store;
 export const
     setSelectedExampleProduct = (product: ExampleProduct | null) =>
-        store.dispatch(mainSlice.actions.setSelectedExampleProduct),
+        store.dispatch(mainSlice.actions.setSelectedExampleProduct(product)),
     addItemToAwaitedProducts = (item: PurcashedProduct) =>
-        store.dispatch(mainSlice.actions.setSelectedExampleProduct),
+        store.dispatch(mainSlice.actions.addItemToAwaitedProducts(item)),
     deleteItemFromAwaitedProducts = (item: PurcashedProduct) =>
-        store.dispatch(mainSlice.actions.deleteItemFromAwaitedProducts),
+        store.dispatch(mainSlice.actions.deleteItemFromAwaitedProducts(item)),
     archiveItem = (item: PurcashedProduct) =>
-        store.dispatch(mainSlice.actions.archiveItem),
+        store.dispatch(mainSlice.actions.archiveItem(item)),
     dearchiveItem = (item: PurcashedProduct) =>
-        store.dispatch(mainSlice.actions.dearchiveItem),
+        store.dispatch(mainSlice.actions.dearchiveItem(item)),
     deleteItemFromArchivedProducts = (item: PurcashedProduct) =>
-        store.dispatch(mainSlice.actions.deleteItemFromArchivedProducts),
+        store.dispatch(mainSlice.actions.deleteItemFromArchivedProducts(item)),
     setCardsView = (useCardsView: boolean) =>
-        store.dispatch(mainSlice.actions.setCardsView),
+        store.dispatch(mainSlice.actions.setCardsView(useCardsView)),
     setSelectedCurrency = (currencyCode: string) =>
-        store.dispatch(mainSlice.actions.setSelectedCurrency);
+        store.dispatch(mainSlice.actions.setSelectedCurrency(currencyCode));
