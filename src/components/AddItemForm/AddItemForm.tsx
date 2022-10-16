9284f14fe864c791bb0c93b63e44761b96d82cd5
NoamAnisfeld <NoamAnisfeld@gmail.com>
2022-10-16 15:10:08 +0300

    merge the AddItemDialog and AddItemForm

--- src/components/AddItemForm/AddItemForm.tsx
+++ src/components/AddItemForm/AddItemForm.tsx
@@ -1,56 +1,123 @@
 import { useState } from 'react'
 
-import { useAppSelector } from '../../GlobalState/interface'
+import PurcashedProduct from '../../data structures/PurcashedProduct'
 
+import { useAppSelector } from '../../GlobalState/interface'
 import {
-    setSelectedExampleProduct,
     addItemToAwaitedProducts,
 } from '../../GlobalState/dispatchers'
 
-import Grid from '@mui/material/Grid'
-import Autocomplete from '@mui/material/Autocomplete'
+import Dialog from '@mui/material/Dialog'
 import TextField from '@mui/material/TextField'
+import Autocomplete from '@mui/material/Autocomplete'
 import Button from '@mui/material/Button'
-import AddItemDialog from '../AddItemDialog/AddItemDialog'
+import Grid from '@mui/material/Grid'
+
+function formatDateToValueString(date: Date) { // yyyy-mm-dd
+    return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+}
 
 export default function AddItemForm() {
-    const exampleProducts = useAppSelector(state => state.exampleProducts);
+    const WEEK = 7 * 24 * 60 * 60 * 1000;
 
-    const [isAddItemDialogOpened, setIsAddItemDialogOpened] = useState(false);
+    const
+        [isAddItemDialogOpened, setIsAddItemDialogOpened] = useState(false),
+        [name, setName] = useState(''),
+        [store, setStore] = useState(''),
+        [price, setPrice] = useState(0),
+        [estimatedDeliveryDate, setEstimatedDeliveryDate] =
+            useState<Date>(new Date(Date.now() + WEEK));
+
+    const exampleProducts = useAppSelector(state => state.exampleProducts);
 
     return <Grid container
         alignItems="center"
         justifyContent="end"
         p={2}
     >
-        <Grid item>
-            <Button variant="contained" sx={{ m: 2 }}
-                onClick={() => setIsAddItemDialogOpened(true)}
-            >
-                + Add item
-            </Button>
-        </Grid>
-        <Grid item>
-            <Autocomplete
-                sx={{ minWidth: "30ch" }}
-                options={Object.values(exampleProducts).map(item => ({
-                    label: `${item.title} (${item.price}$)`,
-                    item
-                })) || []}
-                isOptionEqualToValue={(option, value) => option.item.id === value.item.id}
-                renderInput={params => <TextField {...params} label="or choose from list" />}
-                onChange={(event, value) => {
-                    setSelectedExampleProduct(value?.item || null);
+        <Button variant="contained" sx={{ m: 2 }}
+            onClick={() => setIsAddItemDialogOpened(true)}
+        >
+            + Add item
+        </Button>
 
-                    if (value?.item)
-                        setIsAddItemDialogOpened(true);
-                }}
-            />
-            <AddItemDialog
-                open={isAddItemDialogOpened}
-                onClose={() => setIsAddItemDialogOpened(false)}
-                onSave={newProduct => addItemToAwaitedProducts(newProduct)}
-            />
-        </Grid>
-    </Grid>
+        <Dialog
+            open={isAddItemDialogOpened}
+            onClose={() => setIsAddItemDialogOpened(false)}
+        >
+            <Grid container p={3} spacing={3}>
+                <Grid item flexBasis="50%">
+                    <Autocomplete
+                        freeSolo={true}
+                        options={Object.values(exampleProducts).map(item => ({
+                            label: item.title,
+                            item
+                        })) || []}
+                        isOptionEqualToValue={(option, value) => option.item.id === value.item.id}
+                        renderInput={params =>
+                            <TextField
+                                {...params}
+                                label="Name"
+                            />}
+                        onChange={(event, value) => {
+                            if (typeof value === 'string') {
+                                setName(value)
+                            } else {
+                                setName(value?.item.title || '');
+                                setPrice(price =>
+                                    (typeof value?.item.price === 'number') ?
+                                        value.item.price : price
+                                );
+                            }
+                        }}
+                    />
+                </Grid>
+                <Grid item flexBasis="50%">
+                    <TextField
+                        label="Store"
+                        sx={{ width: "100%" }}
+                        value={store}
+                        placeholder="e.g. Amazon"
+                        onChange={e => setStore(e.target.value)}
+                    />
+                </Grid>
+                <Grid item container flexBasis="50%" alignItems="center">
+                    <span style={{ marginLeft: "-1ch" }}>$</span>
+                    <TextField
+                        label="Price"
+                        type="number"
+                        sx={{ width: "100%" }}
+                        value={String(price)}
+                        onChange={e => setPrice(Number(e.target.value))}
+                    />
+                </Grid>
+                <Grid item flexBasis="50%">
+                    <TextField type="date"
+                        label="Recieve date estimation"
+                        sx={{ width: "100%" }}
+                        value={formatDateToValueString(estimatedDeliveryDate)}
+                        onChange={e => setEstimatedDeliveryDate(new Date(e.target.value))}
+                    />
+                </Grid>
+            </Grid>
+            <Grid container p={2} justifyContent="end" gap={2}>
+                <Button onClick={() => setIsAddItemDialogOpened(false)}>
+                    Cancel
+                </Button>
+                <Button variant="contained"
+                    onClick={() => {
+                        setIsAddItemDialogOpened(false);
+                        addItemToAwaitedProducts(new PurcashedProduct({
+                            name,
+                            store,
+                            price,
+                            estimatedDeliveryDate
+                        }));
+                    }}
+                >
+                    Add
+                </Button>
+            </Grid>
+        </Dialog >
+    </Grid >
 }
