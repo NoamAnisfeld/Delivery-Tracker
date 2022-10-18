8a65d4dd6e381056fd352f01b066a883111092f7
NoamAnisfeld <NoamAnisfeld@gmail.com>
2022-10-19 02:32:43 +0300

    List by store

--- src/components/StoresSummary/StoresSummary.tsx
+++ src/components/StoresSummary/StoresSummary.tsx
@@ -1,22 +1,53 @@
+import useStoresData from './useStoresData';
+import Price from '../Price/Price';
+
 import Grid from '@mui/material/Grid';
 import TableContainer from '@mui/material/TableContainer';
 import Table from '@mui/material/Table';
 import TableHead from '@mui/material/TableHead';
+import TableFooter from '@mui/material/TableFooter';
 import TableBody from '@mui/material/TableBody';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
+import { minHeight } from '@mui/system';
 
 export default function StoresSummary() {
+    const [storesArray, totalAllItems, totalAllPrice] = useStoresData();
+
     return <Grid container justifyContent="center">
         <TableContainer sx={{ width: "auto" }}>
             <Table sx={{ tableLayout: "fixed", width: "auto" }}>
                 <TableHead>
                     <TableRow>
-                        <TableCell>Store</TableCell>
-                        <TableCell>Quantity</TableCell>
-                        <TableCell>Price</TableCell>
+                        <TableCell sx={{ width: "min(20vw, 30ch)" }}>
+                            Store
+                        </TableCell>
+                        <TableCell sx={{ width: "min(20vw, 10ch)" }}>
+                            Quantity
+                        </TableCell>
+                        <TableCell sx={{ width: "min(20vw, 10ch)" }}>
+                            Price
+                        </TableCell>
                     </TableRow>
                 </TableHead>
+                <TableBody>
+                    {storesArray.map(item => <TableRow>
+                        <TableCell>{item.name}</TableCell>
+                        <TableCell>{item.totalItems}</TableCell>
+                        <TableCell>
+                            <Price priceInUSDollars={item.totalPrice} />
+                        </TableCell>
+                    </TableRow>)}
+                </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TableCell>All</TableCell>
+                        <TableCell>{totalAllItems}</TableCell>
+                        <TableCell>
+                            <Price priceInUSDollars={totalAllPrice} />
+                        </TableCell>
+                    </TableRow>
+                </TableFooter>
             </Table>
         </TableContainer>
     </Grid>
---
+++ src/components/StoresSummary/useStoresData.ts
@@ -0,0 +1,70 @@
+import { TupleType } from "typescript";
+import PurcashedProduct from "../../data structures/PurcashedProduct";
+import { useAppSelector } from "../../GlobalState/interface";
+
+interface StoreData {
+    totalItems: number,
+    totalPrice: number,
+}
+
+interface StoreDataWithName extends StoreData {
+    name: string,
+}
+
+export default function useStoresData(): [
+    StoreDataWithName[],
+    number,
+    number,
+] {
+    const
+        awaitedProducts = useAppSelector(state => state.awaitedProducts),
+        archivedProducts = useAppSelector(state => state.archivedProducts);
+    
+    const storesMap: {
+        [key: string | symbol]: StoreData
+    } = {};
+    const keyForUnknownStore = Symbol();
+
+    let totalAllItems = 0,
+        totalAllPrice = 0;
+
+    (<PurcashedProduct[]>[])
+    .concat(awaitedProducts, archivedProducts)
+    .forEach(item => {
+        const key = item.store || keyForUnknownStore;
+
+        if (key in storesMap) {
+            storesMap[key].totalItems++;
+            storesMap[key].totalPrice += item.price;
+        } else {
+            storesMap[key] = {
+                totalItems: 1,
+                totalPrice: item.price
+            }
+        }
+
+        totalAllItems++;
+        totalAllPrice += item.price;
+    })
+
+    const storesArray: StoreDataWithName[] =
+        Object.entries(storesMap).map(([key, value]) => ({
+            name: key,
+            totalItems: value.totalItems,
+            totalPrice: value.totalPrice,
+        }));
+
+    // Object.entries only enumrate string properties
+    if (keyForUnknownStore in storesMap)
+        storesArray.push({
+            name: 'Unknown store',
+            totalItems: storesMap[keyForUnknownStore].totalItems,
+            totalPrice: storesMap[keyForUnknownStore].totalPrice,
+        })
+
+    return [
+        storesArray,
+        totalAllItems,
+        totalAllPrice,
+    ]
+}
