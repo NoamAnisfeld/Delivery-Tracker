f42b98ad8b297896c3e692bd635f5d15022eca68
NoamAnisfeld <NoamAnisfeld@gmail.com>
2022-10-23 10:57:33 +0300

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,23 @@ import PrimaryNavigation from './components/Navigation/PrimaryNavigation';
 import Main from './components/Main/Main';
 import StoresSummary from './components/StoresSummary/StoresSummary';
 
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import {
+  HashRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
-      <HashRouter>
+      <Router>
         <PrimaryNavigation />
         <Routes>
           <Route index path="/*" element={<Main />} />
           <Route path="stores" element={<StoresSummary />} />
         </Routes>
-      </HashRouter>
+      </Router>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import { Provider } from 'react-redux';
 import store from './GlobalState/store';
 import './GlobalState/initStore';
+
+import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <Provider {...{ store }}>
